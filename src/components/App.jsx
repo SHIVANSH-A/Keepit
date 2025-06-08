e89552397c3ae27adc8cf0bf05e8aba3d6a04aef
SHIVANSH-A <shivansh10dubey@gmail.com>
2025-06-08 22:43:21 +0530

    Funtionality enhanced

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,40 @@
-import React , {useState} from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
-  const [notes,setNotes] = useState([]);
-  function addNote(note){
-    setNotes((prevValue) =>{
-      return [...prevValue,note];
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
     });
   }
 
-  function deleteNote(id){
-    setNotes(prevValue=>{
-      return prevValue.filter((value,idx)=>{
-        return idx!==id;
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
       });
-    })
+    });
   }
+
   return (
     <div>
       <Header />
-      <CreateArea onAdd={addNote}/>
-      {notes.map((currNote,index)=>{
-       return <Note key={index} id ={index} title={currNote.title} content={currNote.content} onDelete={deleteNote}/>
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
       })}
       <Footer />
     </div>
